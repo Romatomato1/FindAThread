8c0afa65098467aed18e99346a2625fa09a22e9c
Sanket <83979020+karnasanket@users.noreply.github.com>
2023-07-11 19:30:29 -0700

    Add files via upload
    added error handling to where if the image has already been uploaded, it will let the user know as such and flask wont break

--- upload-image.js
+++ upload-image.js
@@ -1,48 +1,55 @@
 function getFileName() {
-    var x = document.getElementById('fileInput');
-    document.getElementById('fileName').innerHTML = x.value.split('\\').pop();
-  }
-  
-  function uploadImage() {
-    var fileInput = document.getElementById('fileInput');
-    var file = fileInput.files[0];
-  
-    if (file) {
-      var formData = new FormData();
-      formData.append('file', file);
-  
-      var xhr = new XMLHttpRequest();
-      xhr.open('POST', baseUrl + 'upload', true);
-      xhr.onload = function() {
-        if (xhr.status === 200) {
-          // Handle successful upload
-          console.log('File uploaded successfully');
-  
+  var x = document.getElementById('fileInput');
+  document.getElementById('fileName').innerHTML = x.value.split('\\').pop();
+}
+
+function uploadImage() {
+  var fileInput = document.getElementById('fileInput');
+  var file = fileInput.files[0];
+
+  if (file) {
+    var formData = new FormData();
+    formData.append('file', file);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', baseUrl + 'upload', true);
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        var response = JSON.parse(xhr.responseText);
+        if (response.status === 'duplicate') {
+          // Handle duplicate image case
+          Toastify({
+            text: 'Image already uploaded',
+            duration: 3000,
+            gravity: 'top',
+            position: 'left',
+            backgroundColor: 'linear-gradient(to right, #ff4b1f, #ff9068)',
+          }).showToast();
+
+        } else if (response.status === 'success') {
           // Show success message using Toastify
-        Toastify({
-          text: 'File uploaded successfully',
-          duration: 3000,
-          gravity: 'top',
-          position: 'left',
-          backgroundColor: 'linear-gradient(to right, #00b09b, #96c93d)',
-        }).showToast();
-  
-          // Reset file input
-          fileInput.value = null;
-          document.getElementById('fileName').innerHTML = '';
-  
-        } else {
-          // Handle upload error
-          console.error('Error uploading file');
+          Toastify({
+            text: 'File uploaded successfully',
+            duration: 3000,
+            gravity: 'top',
+            position: 'left',
+            backgroundColor: 'linear-gradient(to right, #00b09b, #96c93d)',
+          }).showToast();
         }
-      };
-      xhr.onerror = function() {
+            // Reset file input
+            fileInput.value = null;
+            document.getElementById('fileName').innerHTML = '';
+            
+      } else {
+        // Handle upload error
         console.error('Error uploading file');
-      };
-      xhr.send(formData);
-    } else {
-      console.log('No file selected');
-    }
+      }
+    };
+    xhr.onerror = function () {
+      console.error('Error uploading file');
+    };
+    xhr.send(formData);
+  } else {
+    console.log('No file selected');
   }
-  
-  
+}
